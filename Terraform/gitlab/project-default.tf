16d0ff8a2d9fcba1ad20818c3898075d6a3e5c4f
Davonte Lynton <dlynton01@gmail.com>
2025-03-13 19:52:10 -0700

    updates

--- Terraform/gitlab/project-default.tf
+++ Terraform/gitlab/project-default.tf
@@ -114,3 +114,9 @@ resource "gitlab_project" "import_private" {
     ]
   }
 }
+
+resource "gitlab_deploy_key" "example" {
+  project = "example/deploying"
+  title   = "Example deploy key"
+  key     = "ssh-ed25519 AAAA..."
+}
